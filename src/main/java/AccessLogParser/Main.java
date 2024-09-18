731d7308ff860a26c67ba6e0f453804cf0d991a9
Mikhail Smirnov <misha2548@yandex.ru>
2024-09-18 19:19:29 +0300

    CheckingFile 18/09/2024

--- src/main/java/AccessLogParser/Main.java
+++ src/main/java/AccessLogParser/Main.java
@@ -1,12 +1,31 @@
 package AccessLogParser;
 
+import java.io.File;
 import java.util.Scanner;
 
-
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int counter = 0; // Счётчик корректных файлов
 
-        public static void main(String[] args) {
-            String text = new Scanner(System.in).nextLine();
-            System.out.println("Длина текста: " + text.length());
+        while (true) {
+            System.out.println("Введите путь к файлу");
+            String path = scanner.nextLine();
+
+            File file = new File(path);
+            boolean fileExists = file.exists(); // Проверка существования файла
+            boolean isDirectory = file.isDirectory(); // Проверка, является путь папкой
+
+            // Проверка входных данных
+            if (!fileExists || isDirectory) {
+                System.out.println("Указанный путь не существует или это папка. Попробуйте снова.");
+                System.out.println("Общее количество указанных путей " + counter);
+                continue;
+            }
+            // Если файл существует и это файл
+            counter++;
+            System.out.println("Путь указан верно. Это файл номер " + counter);
         }
     }
+}
+
