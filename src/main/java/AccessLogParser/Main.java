fd8b4b8adc89a1d37cb4c4f88e8368eef2c3750a
Mikhail Smirnov <misha2548@yandex.ru>
2024-10-23 00:02:54 +0300

    Парсер научился непосредственно парсить

--- src/main/java/AccessLogParser/Main.java
+++ src/main/java/AccessLogParser/Main.java
@@ -1,62 +1,115 @@
 package AccessLogParser;
 
-import java.io.BufferedReader;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.LineIterator;
+import java.text.DecimalFormat;
 import java.io.File;
-import java.io.FileReader;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        DecimalFormat df = new DecimalFormat("#.##");
         Scanner scanner = new Scanner(System.in);
         int counter = 0; // Счётчик корректных файлов
 
         while (true) {
             try {
-                System.out.println("Введите путь к файлу");
-                String path = scanner.next(); // Замена nextLine() на next()
+                System.out.println("Введите путь к файлу или введите 'exit', чтобы выйти");
+                String input = scanner.nextLine();
 
-                File file = new File(path);
-                boolean fileExists = file.exists(); // Проверка существования файла
-                boolean isDirectory = file.isDirectory(); // Проверка, является путь папкой
+                if (input.equalsIgnoreCase("exit")) {
+                    System.out.println("Программа завершена.");
+                    break;
+                }
 
-                // Проверка входных данных
+                File file = new File(input);
+                boolean fileExists = file.exists();
+                boolean isDirectory = file.isDirectory();
+
                 if (!fileExists || isDirectory) {
                     System.out.println("Указанный путь не существует или это папка. Попробуйте снова.");
                     System.out.println("Общее количество указанных путей " + counter);
                     continue;
                 }
-                // Если файл существует и это файл
+
                 counter++;
-                System.out.println("Путь указан верно. Это файл номер " + counter);
 
-                FileReader fileReader = new FileReader(path);
-                BufferedReader reader = new BufferedReader(fileReader);
 
-                int totalLines = 0;
-                int maxLength = Integer.MIN_VALUE;
-                int minLength = Integer.MAX_VALUE;
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    totalLines++;
-                    int length = line.length();
-                    if (length > 1024) {
+                LineIterator it = FileUtils.lineIterator(file, "UTF-8");
+                int googlebotCounter = 0;
+                int yandexBotCounter = 0;
+                int lineCounter = 0;
+                while (it.hasNext()) {
+
+                    String line = it.nextLine();
+                    if (line.length() > 1024) {
                         throw new LongException("Длина строки превышает 1024 символа");
                     }
-                    if (length > maxLength) {
-                        maxLength = length;
-                    }
-                    if (length < minLength) {
-                        minLength = length;
-                    }
-                }
+                    String[] parts = line.split(" "); // разделение строки по пробелам
+                    String[] parts1 = line.split("\""); // разделение строки по кавычкам
 
-                System.out.println("Общее количество строк в файле: " + totalLines);
-                System.out.println("Длина самой длинной строки в файле: " + maxLength);
-                System.out.println("Длина самой короткой строки в файле: " + minLength);
+                    String ipAddress = parts[0];
+                    if (ipAddress.length()>0) {
+                        lineCounter = lineCounter +1;
+                    }
+
+                    String dateTime = parts[3] + " " + parts[4].replace("[", "");
+                    String requestMethod = parts[5].replace("\"", "");
+                    String requestPath = parts[6];
+                    int responseCode = Integer.parseInt(parts[8]);
+                    int dataSize = parts[9].equals("-") ? 0 : Integer.parseInt(parts[9]);
+                    String referer = parts.length > 10 ? parts[10].replace("\"", "") : "";
+                    // Индекс начала User-Agent (последняя двойная кавычка перед User-Agent)
+                    int secondLastQuoteIndex = line.substring(0, line.lastIndexOf("\"")).lastIndexOf("\"");
+                    // Извлечение User-Agent начиная с символа следующего после предпоследней двойной кавычки
+                    String userAgent = line.substring(secondLastQuoteIndex);
+
+                    // Вывод данных
+                    System.out.println("IP-адрес: " + ipAddress);
+                    System.out.println("Дата и время запроса: " + dateTime);
+                    System.out.println("Метод запроса: " + requestMethod);
+                    System.out.println("Путь запроса: " + requestPath);
+                    System.out.println("Код ответа: " + responseCode);
+                    System.out.println("Размер данных: " + dataSize);
+                    System.out.println("Referer: " + referer);
+                    System.out.println("User-Agent: " + userAgent);
+                    int startIndex = userAgent.indexOf("(");
+                    int endIndex = userAgent.indexOf(")", startIndex);
+
+                    if (startIndex != -1 && endIndex != -1 && endIndex > startIndex) {
+                        String inParentheses = userAgent.substring(startIndex + 1, endIndex);
+                        //System.out.println("Часть в скобках из User-Agent: " + inParentheses);
+                        String[] parts2 = inParentheses.split(";"); // разделение строки по ;
+                        String prebot = parts2.length > 2 ? parts2[1].replace(" ", ""): "";
+                        String[] parts3 = prebot.split("/");
+                        /*System.out.println("ПреБот :" + prebot);
+                        String bot = parts3.length > 2 ? parts3[1].trim();
+                        System.out.println("Бот :" + bot);*/
+                        if (parts3.length >= 2) {
+                            String bot = parts3[0].trim();
+                            //System.out.println("ПреБот: " + prebot);
+                            System.out.println("Бот: " + bot);
+                            if(bot.equals("Googlebot")) {
+                                googlebotCounter++;
+                            } else {
+                                yandexBotCounter++;
+                            }
+                        }
+                    }
+
+                    System.out.println("--------------------------------------------------");
+                }
+                System.out.println("Общее число строк: " + lineCounter);
+                System.out.println("Кол-во запросов из Яндекса: " + yandexBotCounter);
+                System.out.println("Кол-во запросов из Google: " + googlebotCounter);
+                System.out.println("Доля запросов от Google: " + df.format(((double)googlebotCounter / lineCounter) * 100) + "%");
+                System.out.println("Доля запросов от Яндекса: " + df.format(((double)yandexBotCounter / lineCounter) * 100) + "%");
+                System.out.println("Файл обработан успешно");
+                System.out.println("Путь указан верно. Это файл номер " + counter);
+
             } catch (Exception ex) {
                 ex.printStackTrace();
             }
         }
     }
 }
-
